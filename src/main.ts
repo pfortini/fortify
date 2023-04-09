dffb83da24a949e611834951fd3c8bbd6ec831ac
Pedro Fortini <fortini_ooi@hotmail.com>
2023-04-09 11:22:12 -0300

    project config

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
-if (environment.production) {
+if (environment.production)
   enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(IonicModule.forRoot({})),
-    provideRouter(routes),
-  ],
+    importProvidersFrom(IonicModule.forRoot({}), HttpClientModule),
+    provideRouter(routes)
+  ]
 });
