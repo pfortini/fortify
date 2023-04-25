ad90fc2da0b9fbc34e89700b00bb39e662a49a62
Pedro Fortini <fortini_ooi@hotmail.com>
2023-04-25 12:46:14 -0300

    nicer login page and handle status

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -4,37 +4,36 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { LoginService } from 'src/app/services/login.service';
 import { Router } from '@angular/router';
+import { ProgressIndicatorComponent } from 'src/app/components/progress-indicator/progress-indicator.component';
+import { JsonService } from 'src/app/services/json.service';
 
 @Component({
+  imports: [IonicModule, CommonModule, FormsModule, ProgressIndicatorComponent],
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LoginPage{
 
-  constructor(private router: Router, private loginService: LoginService) {}
+  constructor(private router: Router, private loginService: LoginService, private jsonService: JsonService) {}
 
   async ionViewWillEnter() {
     const params = new URL(window.location.toString()).searchParams;
-    const forceNew = Boolean(params.get('forceNew'));
-
-    if (!forceNew && this.loginService.accessCode && this.loginService.accessToken)
-      return await this.router.navigate(['/home']);
-
     const code = params.get('code');
-    if (code) {
+    const state = params.get('state');
+
+    if (this.loginService.accessCode && this.loginService.accessToken) return await this.router.navigate(['/home']);
+
+    if (code && state) {
+      const storedState = await this.jsonService.readJSON('login_state.json', '');
+      if (state != storedState) return this.router.navigate(['/home']);
+
       this.loginService.setAccessCode(code);
-      params.delete('code');
-      window.history.replaceState({}, document.title, '/login?' + params.toString());
-    } else {
-      // return window.location.assign(this.loginService.spotifyLoginUrl);
-      return console.log('what');
+      await this.loginService.getAccessToken();
     }
 
-    await this.loginService.getAccessToken();
-    await this.router.navigate(['/home']);
+    return await this.router.navigate(['/home']);
   }
 
 }
